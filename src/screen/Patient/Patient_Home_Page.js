bc56cb971eb0a6a81f97a68948eb86b17bb69074
realwhizzylong <therealwhizzy@gmail.com>
2021-10-01 00:09:39 -0400

    patient home

--- src/screen/Patient/Patient_Home_Page.js
+++ src/screen/Patient/Patient_Home_Page.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {Icon, Tab} from "semantic-ui-react";
+import Patient_To_Do from './Patient_To_Do';
+import "../../styles/Patient/Patient_Home_Page.css";
 
 const Patient_Home_Page = () => {
+
+    const panes = [
+        { menuItem: 'To Do', render: () => <Tab.Pane><Patient_To_Do/></Tab.Pane> },
+        { menuItem: 'Completed', render: () => <Tab.Pane>Completed</Tab.Pane> },
+    ]
+
     return (
         <div>
-            <h1>Patient Home Page</h1>
+            <div>
+                <button className="patient_sign_out">Sign out</button>
+                <button className="patient_user">
+                    <Icon name="user patient"/>
+                    Daniel Xiao
+                </button>
+            </div>
+            <div className="patient_panes">
+                {<Tab panes={panes}/>}
+            </div>
         </div>
     )
 }
