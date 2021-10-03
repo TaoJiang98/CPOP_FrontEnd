f815180e0ca14fe59a857054e0659befe24c0f1d
Mikokokokoto <58349362+Mikokokokoto@users.noreply.github.com>
2021-10-02 20:14:26 -0400

    update patient main

--- src/screen/Patient/Patient_Home_Page.js
+++ src/screen/Patient/Patient_Home_Page.js
@@ -1,54 +1,46 @@
-// import React from 'react';
-// import {Icon, Tab} from "semantic-ui-react";
-// import Patient_To_Do from './Patient_To_Do';
-// import "../../styles/Patient/Patient_Home_Page.css";
 import React from 'react';
 import { Box, Button, Card, CardActions, CardContent, CardMedia, Chip, Stack, Typography, Pagination } from '@mui/material';
 
 const Survey_Card_1 = (
-  <Card sx={{ maxWidth: 345 }}>
+  <Card sx={{ maxWidth: 500 }}> 
     <CardMedia
         component="img"
-        alt="green iguana"
+        alt="survey-cover"
         height="140"
-        image="survey-cover.jpg"
+        image="../images/survey-cover.jpg"
     />
     <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-        Lizard
+        Introduction Survey
         </Typography>
         <Typography variant="body2" color="text.secondary">
-        Lizards are a widespread group of squamate reptiles, with over 6,000
-        species, ranging across all continents except Antarctica
+        This survey should take you about 2 minutes.
         </Typography>
     </CardContent>
     <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <Button size="small">Start</Button>
     </CardActions>
     </Card>
   );
 
 const Survey_Card_2 = (
-  <Card sx={{ maxWidth: 345 }}>
+  <Card sx={{ maxWidth: 500 }}>
     <CardMedia
         component="img"
-        alt="green iguana"
+        alt="survey-cover"
         height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
+        image="../images/survey-cover.jpg"
     />
     <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-        Lizard
+        SNOT-22
         </Typography>
         <Typography variant="body2" color="text.secondary">
-        Lizards are a widespread group of squamate reptiles, with over 6,000
-        species, ranging across all continents except Antarctica
+        This survey should take you about 5 minutes.
         </Typography>
     </CardContent>
     <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
+        <Button size="small">Start</Button>
     </CardActions>
     </Card>
     );
