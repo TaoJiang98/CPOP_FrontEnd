2468bc06cfbbe0a26888861f5d8fa760a486fa3c
Mikokokokoto <58349362+Mikokokokoto@users.noreply.github.com>
2021-10-28 22:34:33 -0400

    invite functionality

--- src/screen/Doctor/Doctor_Home_Page.js
+++ src/screen/Doctor/Doctor_Home_Page.js
@@ -3,33 +3,26 @@ import React from "react";
 import ChartistGraph from "react-chartist";
 // @material-ui/core
 import { makeStyles } from "@material-ui/core/styles";
-import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
-import Store from "@material-ui/icons/Store";
-import Warning from "@material-ui/icons/Warning";
-import DateRange from "@material-ui/icons/DateRange";
-import LocalOffer from "@material-ui/icons/LocalOffer";
+
 import Update from "@material-ui/icons/Update";
 import ArrowUpward from "@material-ui/icons/ArrowUpward";
 import AccessTime from "@material-ui/icons/AccessTime";
 import Accessibility from "@material-ui/icons/Accessibility";
-import BugReport from "@material-ui/icons/BugReport";
-import Code from "@material-ui/icons/Code";
-import Cloud from "@material-ui/icons/Cloud";
+
+
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
-import Table from "components/Table/Table.js";
-import Tasks from "components/Tasks/Tasks.js";
-import CustomTabs from "components/CustomTabs/CustomTabs.js";
-import Danger from "components/Typography/Danger.js";
+
+
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardIcon from "components/Card/CardIcon.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
 
-import { bugs, website, server } from "variables/general.js";
+
 
 import {
   dailySalesChart,
@@ -37,7 +30,12 @@ import {
   completedTasksChart,
 } from "variables/charts.js";
 
+import "assets/css/material-dashboard-react.css?v=1.10.0";
+import "assets/jss/material-dashboard-react/layouts/adminStyle.js";
+
 import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
+
+import TopBar from 'components/TopBar_Doctor.js';
 
 const useStyles = makeStyles(styles);
 
@@ -45,6 +43,7 @@ export default function Dashboard() {
   const classes = useStyles();
   return (
     <div>
+      <TopBar/>
       <GridContainer>
         <GridItem xs={12} sm={6} md={3}>
           <Card>
